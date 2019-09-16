89cc4d9bb88581a5f9c92a4bd62382f6d86cb26d
RobertClayton <robbie.c.clayton@gmail.com>
2019-09-16 17:43:59 +0100

    Correcting syntax errors

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -1,15 +1,17 @@
-class BitmapEditor
+# frozen_string_literal: true
 
+# This is the initial interface that the user interacts with
+class BitmapEditor
   def run(file)
-    return puts "please provide correct file" if file.nil? || !File.exists?(file)
+    return puts 'please provide correct file' if file.nil? || !File.exist?(file)
 
     File.open(file).each do |line|
       line = line.chomp
       case line
       when 'S'
-          puts "There is no image"
+        puts 'There is no image'
       else
-          puts 'unrecognised command :('
+        puts 'unrecognised command :('
       end
     end
   end
