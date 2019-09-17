789b8b10ed3a814adef97f6ff12b45ac178dfb96
RobertClayton <robbie.c.clayton@gmail.com>
2019-09-17 20:51:38 +0100

    Testing command edge cases

--- spec/lib/bitmap_editor_spec.rb
+++ spec/lib/bitmap_editor_spec.rb
@@ -4,18 +4,27 @@ require 'spec_helper'
 
 RSpec.describe BitmapEditor do
   describe '#run' do
-    it 'returns when provided a file that does not exist' do
-      expected_warning = 'Please provide a correct file'
+    let(:text_file) { TextFileCreator.new }
+    let(:incorrect_file) { 'Please provide a correct file' }
+    let(:not_recognised) { 'Command not recognised. This command line has been skipped.' }
 
-      expect { subject.run('fake.txt') }.to output(/.+#{expected_warning}$/).to_stdout_from_any_process
+    it 'returns when provided a file that does not exist' do
+      expect { subject.run('fake.txt') }.to output(/.+#{incorrect_file}$/).to_stdout_from_any_process
     end
 
     it 'returns a warning when a command is not recognised' do
-      text_file = TextFileCreator.new
-      text_file.add_command('x')
-      expected_warning = 'Command: x is not recognised. This command line has been skipped.'
+      text_file.add_command('Q')
+      expect { subject.run(text_file.file_path) }.to output(/.+#{not_recognised}$/).to_stdout_from_any_process
+    end
 
-      expect { subject.run(text_file.file_path) }.to output(/.+#{expected_warning}$/).to_stdout_from_any_process
+    it 'returns a warning when a command is more than one character' do
+      text_file.add_command('VV')
+      expect { subject.run(text_file.file_path) }.to output(/.+#{not_recognised}$/).to_stdout_from_any_process
+    end
+
+    it 'returns a warning when a command is not a capital letter' do
+      text_file.add_command('x')
+      expect { subject.run(text_file.file_path) }.to output(/.+#{not_recognised}$/).to_stdout_from_any_process
     end
   end
 end
